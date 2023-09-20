8786f5bc0dfec3f8e61e1e2c9fd0135104e9f5b7
thatsmniti <134503971+thatsmniti@users.noreply.github.com>
2023-09-20 07:54:21 -0300

    Add files via upload

---
+++ condicional/condicional.js
@@ -0,0 +1,107 @@
+function media(){
+
+  var val1 = document.getElementById('valor1').value
+  
+  var val2 = document.getElementById('valor2').value;
+  
+  var media= (parseInt(val1)+ parseInt(val2))/2;
+  
+   
+  
+   
+  
+  if(media>=6)
+  
+  if(media==10)
+  
+      alert("O aluno Atingiua a media" + media + "e foi aprovado");
+  
+  else
+  
+      alert("Parabens, aprovado! Media"+ media);
+  
+  else
+  
+  alert("volta ano que vem! ")
+  
+   
+  
+  }
+  
+   
+  
+  function comparador(){
+  
+   
+  
+   
+  
+   
+  
+      var comp1 =parseInt('valor1').value;
+  
+   
+  
+   
+  
+   
+  
+      var comp2 =parseInt('valor2').value;
+  
+   
+  
+   
+  
+   
+  
+   
+  
+   
+  
+   
+  
+   
+  
+      if(comp1==comp2){alert('os dois numeros são iguais')}
+  
+   
+  
+   
+  
+   
+  
+      else{
+  
+   
+  
+   
+  
+   
+  
+      if(comp1>comp2){alert('o numero ' + comp1 + ' é maior que o ' + comp2)}
+  
+   
+  
+   
+  
+   
+  
+  else{alert('o numero ' + comp1 + ' é menor que o ' + comp2)}
+  
+   
+  
+   
+  
+   
+  
+   
+  
+   
+  
+   
+  
+   
+  
+      } }
+
+
